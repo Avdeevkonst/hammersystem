1ee9498323d03b5e6fa2e3f6c388b7f94ffccb99
Konstantin <hardfiveeven@mail.ru>
2023-08-20 16:58:56 +0300

    added static

--- hammersystem/settings.py
+++ hammersystem/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = "hammersystem.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "static/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -142,7 +142,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = '/myprojects/hammersystem/static/'
+STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
